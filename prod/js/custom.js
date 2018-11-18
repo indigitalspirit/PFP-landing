8b4afd3fbd6f7ca74e864dfbfd6a6f72494d68d6
nastya <nastya-pavlova-93@yandex.ru>
2018-11-18 07:34:17 +0300

    slider added, upload form (action) added, small css changes (footer) added, Y.Metrika and JivoSite counters added

--- prod/js/custom.js
+++ prod/js/custom.js
@@ -50,6 +50,45 @@ $(".project-form__button").click(
     submitUpload();
   });
 
+
+
+
+
+  $(".floors-features .nav-link").click(
+    function(){
+      console.log('tab-click ');
+      var imgName = '';
+      switch(this.id) {
+        case 'floors-oil-tab':
+          imgName = 'oil';
+          break;
+        case 'floors-farm-tab':
+          imgName = 'farm';
+          break;
+        case 'floors-food-tab':
+          imgName = 'food';
+          break;
+        case 'floors-chemistry-tab':
+          imgName = 'chemistry';
+          break;
+        case 'floors-drugs-tab':
+          imgName = 'drugs';
+          break;
+        case 'floors-logs-tab':
+          imgName = 'logs';
+          break;
+        default:
+          imgName = 'oil';
+      }
+
+      $(".floors").css({'background-image': 'url(images/floors/floors-' + imgName + '.png)'});
+      
+      $(".tab-pane.fade").css({'transition': 'opacity .3s'});
+
+   
+      
+      
+    });
     
 
 
